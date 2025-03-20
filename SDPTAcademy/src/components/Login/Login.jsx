abb422d491cc2d1b5e57da63bd3f94ede8a9f685
SamuelAlac <samuelalac21@gmail.com>
2025-03-20 18:39:23 +0800

    papull

--- SDPTAcademy/src/components/Login/Login.jsx
+++ SDPTAcademy/src/components/Login/Login.jsx
@@ -4,19 +4,21 @@ import './login.css'
 
 
 const Login = () => {
-    const [isLogin, setIsLogin] = useState(true);
+    const [isRegistered, setIsRegistered] = useState(true);
 
     const handleChange = () =>{
-        setIsLogin(!isLogin);
+        setIsRegistered(!isRegistered);
     }
 
   return (
     <div className="container vh-100 d-flex align-items-center justify-content-center text-white">
         <form className='p-3 container login-container bg-dark rounded-4'>
+
             <legend className='text-center'>
                 <img src={SDPTLogo} alt="" className="img-fluid" />
                 <h3 className="fw-bold d-inline mx-2">SDPT <span className='lead fs-3'>Academy</span></h3>
             </legend>
+
             <div className="my-3">
                 <label htmlFor="userName" className='form-label fw-semibold fs-5'>Username</label>
                 <input type="text" className="form-control rounded-5" id ="userName"/>
@@ -30,8 +32,24 @@ const Login = () => {
                 <input type="password" className="form-control rounded-5" id ="userPassword"/>
             </div>
 
+            {!isRegistered &&
+            <div className="mb-3">
+                <label htmlFor="userConfirmPassword" className='form-label fw-semibold fs-5'>Confirm Password</label>
+                <input type="password" className="form-control rounded-5" id ="userConfirmPassword"/>
+            </div>
+            }
+
             <div className="text-center">
-                <button className="btnLogin w-100 btn btn-warning btn-md mt-3 rounded-5 text-white">LOGIN</button>
+                <button className="btnLogin w-100 btn btn-warning btn-md mt-3 rounded-5 text-white" onClick={""}>LOGIN</button>
+            </div>
+
+            <div className="text-center mt-3">
+                {isRegistered && 
+                    <p className="lead text-secondary">Don't have an account?<span className='fsw-medium text-white mx-1' role='button' onClick={handleChange}>Sign up</span></p>                
+                }
+                {!isRegistered &&
+                    <p className="lead text-secondary">Already have an account?<span className='fsw-medium text-white mx-1' role='button' onClick={handleChange}>Sign in</span></p>                
+                }
             </div>
         </form>
     </div>
