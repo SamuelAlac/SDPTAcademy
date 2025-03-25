59540d2a5261c01a5757a63c285b544c4ab3db0d
Jimsalm <hypermood15.jimiel@gmail.com>
2025-03-25 18:36:17 +0800

    otp prep

---
+++ SDPTAcademy/src/components/otp/phoneAuth.jsx
@@ -0,0 +1,29 @@
+import React, {useState} from "react";
+import { auth, RecaptchaVerifier, signInWithPhoneNumber } from "../../components/configs/firebase-config";
+
+
+const PhoneAuth = () => {
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [otp, setOtp] = useState("");
+    const [confirmationResult, setConfirmationResult] = useState(null);
+    const [message, setMessage] = useState("");
+
+    const setupRecaptcha = () => {
+        if(!window.recaptchaVerifier){
+            window.recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
+                'size': 'normal',
+                'callback': (response) => {
+                    // reCAPTCHA solved, allow signInWithPhoneNumber.
+                },
+                'expired-callback': () => {
+                    // Response expired. Ask user to solve reCAPTCHA again.
+                }
+        }, auth);
+        }
+    }
+
+
+
+};
+
+export default PhoneAuth;
