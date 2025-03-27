85d9c7361944e61585a5a90b0b0cc39d23f65b1c
ver09 <oliver.baldomero09@gmail.com>
2025-03-28 00:32:43 +0800

    f

--- SDPTAcademy/src/Pages/ProfileDashboard/ProfileDashboard.jsx
+++ SDPTAcademy/src/Pages/ProfileDashboard/ProfileDashboard.jsx
@@ -25,7 +25,7 @@ const ProfileDashboard = () => {
           {/* Profile Picture */}
           <div className='col col-md-4 col-sm-12'> 
             <div className="card card-design profile-card text-white text-center align-items-center justify-content-center">
-              <div className="content pt-3 text-warning"><h2>Profile Picture</h2></div>
+              <div className="content pt-3 text-warning"><h2 className='fw-bold'>Profile Picture</h2></div>
               <img src={user?.photoURL || '/default-avatar.png'} alt="Profile" className="profile-picture mt-4 mb-4" />
               <p className='mt-2 mb-4'>JPG or PNG no longer than 5MB</p>
               <button className="btn btn-warning mb-5">Upload New Image</button>
@@ -33,7 +33,7 @@ const ProfileDashboard = () => {
 
             {/* Change Password */}
             <div className="card card-design password-card text-white text-center align-items-center justify-content-center pb-5">
-              <div className="content pt-3 mb-4 text-warning"><h2>Change Password</h2></div>
+              <div className="content pt-3 mb-4 text-warning"><h2 className='fw-bold'>Change Password</h2></div>
               <div className='input-fields justify-content-center align-items-center'>
               <div className=" mb-md-3 px-3">
                 <label className='form-label fw-semibold fs-5'>Current Password</label>
@@ -55,7 +55,7 @@ const ProfileDashboard = () => {
 
           {/* Account Details */}
           <div className="col card card-design account-card text-white text-left align-items-center">
-            <div className="content pt-3 mt-5 text-warning"> <h2>Account Details</h2></div>
+            <div className="content pt-3 mt-5 text-warning"> <h2 className='fw-bold'>Account Details</h2></div>
             <div className='input-fields justify-content-center align-items-center mt-5'>
             <div className="row px-3">
               <div className="col mb-md-3">
