c316414695e864d5c4d5cade873f8dce30bee767
SamuelAlac <samuelalac21@gmail.com>
2025-03-28 00:01:51 +0800

    playlist

--- SDPTAcademy/src/Pages/Courses/CoursePlaylist.jsx
+++ SDPTAcademy/src/Pages/Courses/CoursePlaylist.jsx
@@ -4,10 +4,18 @@ import Navbar from '../../components/Header/Navbar'
 const CoursePlaylist = () => {
   return (
     <div>
-        <Navbar className="bg-black border-bottom rounded-bottom-4 border-effect" />
+        <Navbar className="bg-black border-bottom rounded-bottom-4" />
 
         <section className="fixed-layout vh-100 bg-white">
-            
+          <div className="container-fluid">
+            <div className="row row-cols-3 text-center">
+              <div className='col-md bg-black'>
+                
+              </div>
+              <div className='col-md-6'>sam</div>
+              <div className='col-md'>sam</div>
+            </div>
+          </div>
         </section>
     </div>
   )
