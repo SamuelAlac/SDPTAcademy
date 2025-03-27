236974c46d9ef38b60ee27e4bb2a24fb9c50dc80
ver09 <oliver.baldomero09@gmail.com>
2025-03-27 22:01:56 +0800

    profile dashboard cards layout

--- SDPTAcademy/src/Pages/ProfileDashboard/ProfileDashboard.jsx
+++ SDPTAcademy/src/Pages/ProfileDashboard/ProfileDashboard.jsx
@@ -3,11 +3,28 @@ import Navbar from '../../components/Header/Navbar'
 import './ProfileDashboard.css'
 
 
+
 const ProfileDashboard = () => {
   return (
     <>
-        <Navbar/>
-        
+    <Navbar/>
+    <div className="dashboard-container ">
+      <div className="container-fluid ">
+        <div className="row cards-container">
+          <div className='col col-md-4 col-sm-12'>
+          <div className=" profile-card">
+            <div className="content">Profile Picture</div>
+          </div>
+          <div className="password-card">
+            <div className="content">Change Password</div>
+          </div>
+          </div>    
+          <div className="col  account-card">
+            <div className="content">Account Details</div>
+          </div>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
