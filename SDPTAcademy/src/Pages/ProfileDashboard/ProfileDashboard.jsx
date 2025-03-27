e628e240ce8bd265ecf88cab09800137ab68cae5
ver09 <oliver.baldomero09@gmail.com>
2025-03-27 13:58:24 +0800

    f

--- SDPTAcademy/src/Pages/ProfileDashboard/ProfileDashboard.jsx
+++ SDPTAcademy/src/Pages/ProfileDashboard/ProfileDashboard.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import Navbar from '../../components/Header/Navbar'
 import './ProfileDashboard.css'
-import SDPTLogo from '../../assets/SDPTLogo_Big.svg'
-import googleTranslate from '../../assets/Google Translate.svg'
 
 
 const ProfileDashboard = () => {
