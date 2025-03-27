f2d77938e9f923864d1b856edb25eb315ac9fb43
ver09 <oliver.baldomero09@gmail.com>
2025-03-27 22:33:23 +0800

    profile dash cards

--- SDPTAcademy/src/Pages/ProfileDashboard/ProfileDashboard.jsx
+++ SDPTAcademy/src/Pages/ProfileDashboard/ProfileDashboard.jsx
@@ -12,15 +12,15 @@ const ProfileDashboard = () => {
       <div className="container-fluid ">
         <div className="row cards-container">
           <div className='col col-md-4 col-sm-12'>
-          <div className=" profile-card">
-            <div className="content">Profile Picture</div>
+          <div className="card card-design profile-card text-white text-center">
+            <div className="content pt-3">Profile Picture</div>
           </div>
-          <div className="password-card">
-            <div className="content">Change Password</div>
+          <div className="card card-design password-card text-white text-center">
+            <div className="content pt-3">Change Password</div>
           </div>
           </div>    
-          <div className="col account-card">
-            <div className="content">Account Details</div>
+          <div className="col card card-design account-card text-white text-center">
+            <div className="content pt-3">Account Details</div>
           </div>
         </div>
       </div>
