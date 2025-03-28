a3a1b0d405d9fd53e755fd2fa35f95584efe41c6
SamuelAlac <samuelalac21@gmail.com>
2025-03-28 08:56:58 +0800

    Course Playlist Pa pull

--- SDPTAcademy/src/Pages/Courses/CoursePlaylist.jsx
+++ SDPTAcademy/src/Pages/Courses/CoursePlaylist.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../../components/Header/Navbar'
 import './CoursePlaylist.css'
 
 const CoursePlaylist = () => {
+
+  const [embeddedVideo, setEmbeddedVideo] = useState("https://www.youtube.com/embed/8HXqOHH2ocM")
+
+  const changeEmbeddedVideo = (videoID) => {
+    setEmbeddedVideo(`https://www.youtube.com/embed/${videoID}`)
+  }
+
   return (
     <div>
         <Navbar className="bg-black border-bottom rounded-bottom-3 border-effect" />
@@ -15,28 +22,40 @@ const CoursePlaylist = () => {
 
                 <ul className='list-unstyled'>
                   <li>
-                    <p className='text-warning text-opacity-75'>I: Hello World</p>
+                    <p className='text-warning text-opacity-75 embedded-button-effect' role='button' onClick={() => changeEmbeddedVideo("8HXqOHH2ocM")}>I: Hello World</p>
                   </li>
                   <li>
-                    <p className='text-warning'>II: Hello World Advance Skirtt</p>
+                    <p className='text-warning embedded-button-effect' role='button' onClick={() => changeEmbeddedVideo("lePkYEGt2es")}>II: Variables & Datatype</p>
                   </li>
                   <li>
-                    <p>II: Hello World Advance Skirtt</p>
+                    <p className='embedded-button-effect' role='button' onClick={() => changeEmbeddedVideo("76b2d6ozN_k")}>III: User Input & Arithmetic Operations</p>
                   </li>
                   <li>
-                    <p>II: Hello World Advance Skirtt</p>
+                    <p className='embedded-button-effect' role='button' onClick={() => changeEmbeddedVideo("LzeUR6JJBZo")}>IV: Arrays</p>
                   </li>
                   <li>
-                    <p>II: Hello World Advance Skirtt</p>
+                    <p className='embedded-button-effect' role='button' onClick={() => changeEmbeddedVideo("m8Qo5vT-5MY")}>V: Conditional Statements</p>
                   </li>
                   <li>
-                    <p>II: Hello World Advance Skirtt</p>
+                    <p className='embedded-button-effect' role='button' onClick={() => changeEmbeddedVideo("Px3J6ZdDqls")}>VI: Switch Statements</p>
                   </li>
                   <li>
-                    <p>II: Hello World Advance Skirtt</p>
+                    <p className='embedded-button-effect' role='button' onClick={() => changeEmbeddedVideo("WX978uCVAkM")}>VII: While & Do - While Loop</p>
                   </li>
                   <li>
-                    <p>II: Hello World Advance Skirtt</p>
+                    <p className='embedded-button-effect' role='button' onClick={() => changeEmbeddedVideo("re6AlB8AciI")}>VIII: For Loop</p>
+                  </li>
+                  <li>
+                    <p className='embedded-button-effect' role='button' onClick={() => changeEmbeddedVideo("rGVpVP9o6ps")}>IX: For Each Loop</p>
+                  </li>
+                  <li>
+                    <p className='embedded-button-effect' role='button' onClick={() => changeEmbeddedVideo("zC8R_JXiqDs")}>X: 2D Array + Nested For Loop</p>
+                  </li>
+                  <li>
+                    <p className='embedded-button-effect' role='button' onClick={() => changeEmbeddedVideo("s7GQ5HmlGQY")}>XI: Methods</p>
+                  </li>
+                  <li>
+                    <p className='embedded-button-effect' role='button' onClick={() => changeEmbeddedVideo("VI9D3_zGG74")}>XII: Packages & Modifiers</p>
                   </li>
                 </ul>
 
@@ -44,7 +63,7 @@ const CoursePlaylist = () => {
 
               <div className='container col-md-8'>
                 <div className="ratio ratio-16x9">
-                  <iframe src="https://www.youtube.com/embed/8HXqOHH2ocM?si=UdG60ST60V7t80--" title='Advanced Java Tutorial' allowFullScreen></iframe>
+                  <iframe src={embeddedVideo} title='Advanced Java Tutorial' allowFullScreen></iframe>
 
                 </div>
               </div>
